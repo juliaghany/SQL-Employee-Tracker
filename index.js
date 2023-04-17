19a6381b4b257214a4f206f3fbb83c18f4f353ab
Jules <juliaghany@msn.com>
2023-04-17 14:10:56 -0500

    add removeEmployee() function

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
 require('dotenv').config()
 const helpers = require('./helpers/helpers.js');
-// var db;
+var db;
 
 const connect = async () => {
     try {
         db = await mysql.createConnection({
-            host: 'localhost',
+            host: "localhost",
             port: 3306,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
@@ -21,37 +21,40 @@ const connect = async () => {
 
 async function employeeManager() {
     const answers = await inquirer.prompt([{
-        type: 'list',
-        name: 'prompt',
-        message: 'What would you like to do?',
-        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+        type: "list",
+        name: "prompt",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "Add Employee", "Update Employee Role", "Remove Employee", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"]
     }]);
 
     const selectedOption = answers.prompt
 
     switch (selectedOption) {
-        case 'View All Employees':
+        case "View All Employees":
             await helpers.viewAllEmployees(); 
             break; 
-        case 'Add Employee':
+        case "Add Employee":
             await helpers.addEmployee();
             break;
-        case 'Update Employee Role':
+        case "Update Employee Role":
             await helpers.updateEmployeeRole();
             break;
-        case 'View All Roles':
+        case "Remove Employee":
+            await helpers.removeEmployee();
+            break;
+        case "View All Roles":
             await helpers.viewAllRoles();
             break;
-        case 'Add Role':
+        case "Add Role":
             await helpers.addRole();
             break;
-        case 'View All Departments':
+        case "View All Departments":
             await helpers.viewAllDepartments();
             break;
-        case 'Add Department':
+        case "Add Department":
             await helpers.addDepartment();
             break;
-        case 'Quit':
+        case "Quit":
             await helpers.quitEmployeeManager();
             break;
     }
