0b0a7acbaa550157f91cc6763b24706d465d00ef
Jules <juliaghany@msn.com>
2023-04-18 12:47:00 -0500

    add comments

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,15 +1,24 @@
+// import required modules 
+
 const db = require('../db/db.js');
 const inquirer = require('inquirer');
-const { ListQuestion, InputQuestion } = require("./questionClass.js")
+
+// import question classes for inquirer prompts
+
+const { ListQuestion, InputQuestion } = require("./questionClass.js"); 
+
+// function to view all employees
 
 const viewAllEmployees = async () => {
     try {
         const [rows, fields] = await db.query("SELECT employee.id, employee.first_name, employee.last_name, roles.title AS role_title, department.department_name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name FROM employee JOIN roles ON employee.role_id = roles.id JOIN department ON roles.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id ORDER BY employee.id ASC");
         console.table(rows);
     } catch (error) {
-        console.error('Failed to fetch employees', error);
+        console.error(`Failed to fetch employees`, error);
     }
 };
+
+// function to add an employee
 
 const addEmployee = async () => {
     try {
@@ -25,12 +34,14 @@ const addEmployee = async () => {
         if (result.affectedRows === 1) {
             console.log(`Added ${answers.first_name + " " + answers.last_name} to database`);
         } else {
-            console.log(`Could not add ${answers.first_name + " " + answers.last_name} to database`)
+            console.log(`Could not add ${answers.first_name + " " + answers.last_name} to database`);
         }
     } catch (error) {
-        console.error("Failed to add employee", error)
+        console.error(`Failed to add employee`, error);
     }
 };
+
+// function to update an employee's role
 
 const updateEmployeeRole = async () => {
 
@@ -44,14 +55,16 @@ const updateEmployeeRole = async () => {
         if (result.affectedRows >= 1) {
             console.log(`Updated employee's role`);
         } else {
-            console.log(`Could not update employee's role`)
+            console.log(`Could not update employee's role`);
         }
     }
 
     catch (error) {
-        console.log("Failed to update employee's role", error);
+        console.log(`Failed to update employee's role`, error);
     }
 };
+
+// function to remove an employee
 
 const removeEmployee = async () => {
     try {
@@ -67,22 +80,26 @@ const removeEmployee = async () => {
         }
 
     } catch (error) {
-        console.log("Failed to remove employee", error);
+        console.log(`Failed to remove employee`, error);
     }
 };
+
+// function to view all roles
 
 const viewAllRoles = async () => {
     try {
-        const [rows, fields] = await db.query('SELECT roles.id, roles.title, department.department_name, roles.salary FROM roles JOIN department ON roles.department_id = department.id');
+        const [rows, fields] = await db.query(`SELECT roles.id, roles.title, department.department_name, roles.salary FROM roles JOIN department ON roles.department_id = department.id`);
         console.table(rows);
     } catch (error) {
-        console.error('Failed to fetch roles', error);
+        console.error(`Failed to fetch roles`, error);
     }
 };
 
+// function to add a role 
+
 const addRole = async () => {
     try {
-        const departments = await db.query("SELECT id AS value, department_name AS name FROM department")
+        const departments = await db.query(`SELECT id AS value, department_name AS name FROM department`)
         const answers = await inquirer.prompt([
             new InputQuestion("role", "Please enter the title of the role:"),
             new InputQuestion("salary", "Please enter the salary for this role:"),
@@ -94,18 +111,22 @@ const addRole = async () => {
             console.log(`Added ${answers.role} to database`);
         }
     } catch (error) {
-        console.error("Failed to add role", error);
+        console.error(`Failed to add role`, error);
     }
 };
 
+// function to view all departments
+
 const viewAllDepartments = async () => {
     try {
-        const [rows, fields] = await db.query("SELECT id, department_name FROM department");
+        const [rows, fields] = await db.query(`SELECT id, department_name FROM department`);
         console.table(rows);
     } catch (error) {
-        console.error("Failed to fetch departments", error);
+        console.error(`Failed to fetch departments`, error);
     }
 };
+
+// function to add a department
 
 const addDepartment = async () => {
 
@@ -120,38 +141,46 @@ const addDepartment = async () => {
     if (result.affectedRows === 1) {
         console.log(`Added ${departmentName} to database`);
     } else {
-        console.log("Failed to add department")
+        console.log(`Failed to add department`);
     }
 };
+
+// function to end the application when user selects quit 
 
 const quitEmployeeManager = async () => {
     try {
-        console.log("Goodbye!");
+        console.log(`Goodbye!`);
         process.exit();
     } catch (error) {
-        console.error("Failed to exit", error)
+        console.error(`Failed to exit`, error);
     }
 };
 
+// helper function for getting roles 
+
 const getRoles = async () => {
     try {
-        const roles = await db.query("SELECT title AS name, id AS value FROM roles")
+        const roles = await db.query(`SELECT title AS name, id AS value FROM roles`)
         return roles[0]
     } catch (error) {
-        console.error("Failed to fetch roles", error);
+        console.error(`Failed to fetch roles`, error);
         return [];
     }
-}
+};
+
+// helper function for getting employees 
 
 const getEmployees = async () => {
     try {
-        const employee = await db.query("SELECT CONCAT(first_name, ' ', last_name) AS name, id AS value FROM employee")
+        const employee = await db.query(`SELECT CONCAT(first_name, ' ', last_name) AS name, id AS value FROM employee`)
         employee[0].push({ name: "None", value: null });
         return employee[0];
     } catch (error) {
-        console.error("Failed to fetch employees", error);
+        console.error(`Failed to fetch employees`, error);
         return [];
     }
-}
+};
+
+// exports all of the helper functions to be used in index.js file
 
 module.exports = { viewAllEmployees, addEmployee, updateEmployeeRole, removeEmployee, viewAllRoles, addRole, viewAllDepartments, addDepartment, quitEmployeeManager, getRoles, getEmployees }
